922285349588b88fe568ee6f1e4a59fde2eb478b
alex blade <aalexblade@gmail.com>
2026-01-22 17:18:58 +0200

    fix pathname

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,7 +5,7 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-   const pathname = usePathname();
+  const pathname = usePathname();
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
@@ -18,8 +18,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`);
   }
 
-
-
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -31,9 +29,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-          defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-} 
+}
